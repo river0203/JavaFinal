e1b91af6d2f3eb3f142072414da2a940d43bf9c1
river0203 <aspp3666@gmail.com>
2024-06-07 15:09:17 +0900

    data Model

--- JavaFinal/src/DrawController.java
+++ JavaFinal/src/DrawController.java
@@ -1,13 +1,19 @@
 import java.awt.Color;
+import java.util.ArrayList;
 import java.awt.Graphics;
 import javax.swing.JPanel;
 
 public class DrawController extends JPanel {
 
+	private SimplePainterModel nowData;
+	private ArrayList<SimplePainterModel> savedList;
+	
 	public DrawController()
 	{
 		setBackground(Color.white);
 		
+		nowData = new SimplePainterModel();
+		savedList = new ArrayList<SimplePainterModel>();
 	}
 	
 	public void paintComponent(Graphics page)
---
+++ JavaFinal/src/SimplePainterModel.java
@@ -0,0 +1,31 @@
+import java.awt.Color;
+import java.awt.Point;
+
+public class SimplePainterModel {
+
+	public int 		nDrawMode;
+	public Point	 ptOne, ptTwo;
+	public int 		nSize;
+	public boolean 	bFill;
+	public Color 	selectedColor;
+	
+	public SimplePainterModel()
+	{
+		nDrawMode = Constants.NONE;
+		ptOne = new Point();
+		ptTwo = new Point();
+		nSize = 1;
+		bFill = false;
+		selectedColor = Color.black;
+	}
+	
+	public SimplePainterModel(SimplePainterModel data)
+	{
+		nDrawMode = data.nDrawMode;
+		ptOne = data.ptOne;
+		ptTwo = data.ptTwo;
+		nSize = data.nSize;
+		bFill = data.bFill;
+		selectedColor = data.selectedColor;
+	}
+}
