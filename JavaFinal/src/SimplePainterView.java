8616e1c108f3ea52eab7c34333d70a3ec01c06aa
river0203 <aspp3666@gmail.com>
2024-06-09 19:39:42 +0900

    update

--- JavaFinal/src/SimplePainterView.java
+++ JavaFinal/src/SimplePainterView.java
@@ -5,7 +5,6 @@ import java.awt.event.*;
 public class SimplePainterView extends JPanel {
 	
 	private DrawController drawController;
-	
 	private JPanel 		menuPanel, optionPanel, messagePanel;
 	private JButton[] 	btnMenuArray;
 	private JTextField 	txtSize;
